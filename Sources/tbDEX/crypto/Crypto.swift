0b43308b8cabeac68f3ea7a5b5846b892ff4316b
Adam Mika <amika@squareup.com>
2023-12-13 20:29:44 -0700

    Remove the existing code, will make further code reviews much easier

--- Sources/tbDEX/crypto/Crypto.swift
+++ Sources/tbDEX/crypto/Crypto.swift
@@ -1,8 +1,1 @@
-public enum Crypto {
-    static func generatePrivateKey(keyType: KeyType) throws -> any PrivateKey {
-        switch keyType {
-        case .ed25519: return Ed25519PrivateKey()
-        case .secp256k1: return try Secp256k1PrivateKey()
-        }
-    }
-}
+public enum Crypto {}
