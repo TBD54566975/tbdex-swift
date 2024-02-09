5bcf7830da7a49a46ca9839b2a932acb2734233c
Adam Mika <amika@squareup.com>
2024-02-09 08:00:13 -0700

    Fix a digest generation issue, this will soon move to web5-swift as well

--- Sources/tbDEX/Protocol/CryptoUtils.swift
+++ Sources/tbDEX/Protocol/CryptoUtils.swift
@@ -9,13 +9,8 @@ enum CryptoUtils {}
 extension CryptoUtils {
 
     static func digest<D: Codable, M: Codable>(data: D, metadata: M) throws -> Data {
-        let encoder = JSONEncoder()
-        encoder.dateEncodingStrategy = .iso8601
-        encoder.outputFormatting = .sortedKeys
-
         let payload = DigestPayload(data: data, metadata: metadata)
-        let serializedPayload = try encoder.encode(payload)
-
+        let serializedPayload = try tbDEXJSONEncoder().encode(payload)
         let digest = SHA256.hash(data: serializedPayload)
         return Data(digest)
     }
