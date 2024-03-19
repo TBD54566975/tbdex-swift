9ea527237f3f53dabfaa129c994796ebe0da983e
kirahsapong <102400653+kirahsapong@users.noreply.github.com>
2024-03-19 10:03:18 -0700

    update typo

--- Sources/tbDEX/Protocol/Models/Messages/OrderStatus.swift
+++ Sources/tbDEX/Protocol/Models/Messages/OrderStatus.swift
@@ -10,7 +10,7 @@ public struct OrderStatusData: MessageData {
     /// Current status of Order that's being executed
     public let orderStatus: String
 
-    /// Returns the MessageKind of kind
+    /// Returns the MessageKind of orderstatus
     public func kind() -> MessageKind {
         return .orderStatus
     }
