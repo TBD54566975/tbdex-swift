27267aa9081942315e0f1b3547eec45daa02c65b
Kirah Sapong <kirah@tbd.email>
2024-03-25 18:09:09 -0700

    update balanceData shape

--- Sources/tbDEX/Protocol/Models/Resources/Balance.swift
+++ Sources/tbDEX/Protocol/Models/Resources/Balance.swift
@@ -8,22 +8,13 @@ public typealias Balance = Resource<BalanceData>
 public struct BalanceData: ResourceData {
 
     /// ISO 3166 currency code string
-    public let balances: [BalanceDetails]
+    public let currencyCode: String
+
+    /// The amount available to be transacted with
+    public let available: String
 
     /// Returns the ResourceKind of balance
     public func kind() -> ResourceKind {
         return .balance
     }
-}
-
-/// Details about a balance associated with a given currency
-///
-/// [Specification Reference](https://github.com/TBD54566975/tbdex/tree/main/specs/protocol#balance)
-public struct BalanceDetails: Codable, Equatable {
-
-    /// ISO 3166 currency code string
-    public let currencyCode: String
-
-    /// The amount available to be transacted with
-    public let available: String
 }
