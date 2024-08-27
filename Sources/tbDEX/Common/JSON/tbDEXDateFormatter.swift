ebd9f0b9a261db076195f9edc3fb6bcd60cffdcb
Ethan Lee <ethanl@tbd.email>
2024-08-27 12:38:54 -0700

    add fallback date formatter to handle decoding dates without fractional seconds

--- Sources/tbDEX/Common/JSON/tbDEXDateFormatter.swift
+++ Sources/tbDEX/Common/JSON/tbDEXDateFormatter.swift
@@ -7,3 +7,12 @@ let tbDEXDateFormatter: ISO8601DateFormatter = {
     dateFormatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
     return dateFormatter
 }()
+
+
+/// A date formatter that can be used to decode dates in the ISO8601 format
+/// without fractional seconds.
+let tbDEXFallbackDateFormatter: ISO8601DateFormatter = {
+    let dateFormatter = ISO8601DateFormatter()
+    dateFormatter.formatOptions = [.withInternetDateTime]
+    return dateFormatter
+}()
